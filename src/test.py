06d3f738bc3f582d2f37b06e7f8096ed9c468a59
Axel <nigthmaresans2@gmail.com>
2024-11-23 14:47:22 -0600

    i'm rewritting everything, i finally understand opencv and qrcode detection

---
+++ src/test.py
@@ -0,0 +1,23 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+decoder = cv2.QRCodeDetector()
+while True:
+    _, frame = cam.read()
+    ret, points = decoder.detect(frame)
+
+    if ret is True:
+        points = points[0].astype(int)
+        for i in range(4):
+            start = tuple(points[i])
+            end = tuple(points[(i + 1) % 4])
+            cv2.line(frame, start, end, (0,0,255), 3)
+
+    cv2.imshow('Camera', frame)
+
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
