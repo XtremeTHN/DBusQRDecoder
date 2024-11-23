88d62a9436f11982f9c6b5eab14fbc71b1e86292
Axel <nigthmaresans2@gmail.com>
2024-11-23 15:06:52 -0600

    implemented: decoding qr codes in test.py

--- src/test.py
+++ src/test.py
@@ -7,6 +7,13 @@ while True:
     ret, points = decoder.detect(frame)
 
     if ret is True:
+        try:
+            content, _ = decoder.decode(frame, points)
+        except cv2.Error as e:
+            print("WARN: Error when detecting qr code from camera:", e, "\nINFO: Ignoring...")
+            continue
+        if content != "":
+            print(content)
         points = points[0].astype(int)
         for i in range(4):
             start = tuple(points[i])
@@ -14,7 +21,6 @@ while True:
             cv2.line(frame, start, end, (0,0,255), 3)
 
     cv2.imshow('Camera', frame)
-
 
     if cv2.waitKey(1) == ord('q'):
         break
